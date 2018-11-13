1f80a08aa20d62032bb0a50e770edbea57ae8d44
HydroLancer <36414472+HydroLancer@users.noreply.github.com>
2018-11-13 01:14:30 +0000

    Importing and exporting working
    SLL functionality fine
    working on Filter() definition and hoo boy it hurts my head.

--- FilterApplication/FilterApplication/filter.h
+++ FilterApplication/FilterApplication/filter.h
@@ -7,11 +7,21 @@
 #define WCHAR char
 #endif
 #include <string>
+
+#ifndef ASEXPORT
+#define DLLIMPORTOREXPORT dllimport
+#else
+#define DLLIMPORTOREXPORT dllimport
+#endif
+
 //Error codes
 
 using namespace std;
 
-int stack = 0;
+extern string outputFile;
+extern string inputFile;
+extern int stack;
+
 const int Success = 0;
 const int Failure = -1;
 const int DllNotFound = -2;		// can't find dll to import
@@ -19,15 +29,18 @@ const int DllInvalidFormat = -3;	// Unable to find import function in the dll
 const int UnknownError = -4;
 const int InvalidParameters = -5;
 
+const int MAX_DATA = 1000;  // the maximum size of the array for loading data 
+const int DATE_SIZE = 19;  // the maximum size of a date as a null-terminated string
+
 //in SLL
 extern int UseFilter(WCHAR* dllName, int data[], int count, WCHAR* parameterString);	//load DLL at Runtime from file and process data stored in the array using Filter() function FROM THE DLL
 																				//passes the parameter string each time the function is called
 
 extern int LoadMeasurements(string inputFile, int data[], int maxSize);				//Reads data from inputFile into array
 
-extern int SaveMeasurements(WCHAR* outputFile, int data[], int maxSize);				//outputs data from the array into an output file
+extern int SaveMeasurements(string outputFile, int data[], int maxSize);				//outputs data from the array into an output file
 
 //in DLL
-int Filter(int data[], int count, WCHAR* parameterString);						//applies algorithm to given data (this function is in DynamicLib)
+_declspec(DLLIMPORTOREXPORT) int Filter(int data[], int count, WCHAR* parameterString);						//applies algorithm to given data (this function is in DynamicLib)
 
 #endif
--- FilterApplication/StaticLib/filtering.cpp
+++ FilterApplication/StaticLib/filtering.cpp
@@ -2,10 +2,31 @@
 #include <fstream>
 #include <iostream>
 #include "filter.h"
+#include <Windows.h>
+using namespace std;
 
-int UseFilter(WCHAR* dllName, int data[], int count, WCHAR* parameterString)
+int UseFilter(WCHAR* dllName, int data[], int count, string parameterString)
 {
-	return 0;
+	typedef int(*FilterProc)(int[], int, string);
+	HINSTANCE loadLib = LoadLibrary(dllName);
+	FilterProc ProcAdd;
+
+	if (loadLib != NULL)
+	{
+		ProcAdd = (FilterProc)GetProcAddress(loadLib, "Filter");
+
+		if (ProcAdd != NULL)
+		{
+			ProcAdd(data, count, parameterString);
+			return Success;
+		}
+		return DllInvalidFormat;
+	}
+	else
+	{
+		return DllNotFound;
+	}
+	return UnknownError;
 }
 
 int LoadMeasurements(string inputFile, int data[], int maxSize)
@@ -13,26 +34,43 @@ int LoadMeasurements(string inputFile, int data[], int maxSize)
 	ifstream input(inputFile);
 	if (!input)
 	{
-		cout << "Error Code: " << InvalidParameters;
+		cout << "Error Code: " << InvalidParameters << endl;
+		return InvalidParameters;
 	}
 
-	while (!input.eof())
+	while (!input.eof())				//makes sure it's only going to run the loop if a file actually loads
 	{
-		for (int i = 0; i < maxSize; i++)
+		for (int i = 0; i < MAX_DATA; i++)
 		{
 			input >> data[i];
-			stack++;
+			stack++;					//used for determining how much data is actually in the array, rather than going through the entire empty array. 
+			if (input.eof())
+			{
+				return Success;			//just stops the loop. 
+			}
 		}
 	}
-	return 0;
+	return UnknownError; //if it fails to run the loop, the file doesn't end for some reason, yet the file loads, gives this. 
 }
 
-int Filter(int data[], int count, WCHAR* parameterString)
+int SaveMeasurements(string outputFile, int data[], int maxSize)
 {
-	return 0 ;
-}
-
-int SaveMeasurements(WCHAR* outputFile, int data[], int maxSize)
-{
-	return 0;
+	ofstream output(outputFile);
+	if (!output)
+	{
+		cout << "Error Code: " << InvalidParameters << endl;
+		return InvalidParameters;
+	}
+	else
+	{
+		output << "Filtered Array: " << endl;
+		for (int i = 0; i < maxSize; i++)
+		{
+			output << data[i] << endl;
+		}
+		output.close();
+		return Success;
+	}
+	return UnknownError;
+	
 }
